e0c1f2784f789c666ac6608c8cd6a1d98b276222
Bakir-Bhuyain <hemalbakir@gmail.com>
2026-01-20 11:24:03 +0600

    finding the small value from an array by using for and for of loop

--- d2_pb01.js
+++ d2_pb01.js
@@ -4,3 +4,28 @@
 // Input: [34, 15, 88, 2, 55]
 
 // Output: 2
+function smallestNumber(numbers) {
+  let small =numbers [0];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i]< small){
+      small = numbers[i];
+    }
+    
+  }
+  return small;
+}
+// const input = [34, 15, 88, 2, 55];
+// const little = smallestNumber(input);
+// console.log(little);
+function smallestNumber(numbers){
+      let small = numbers[0];
+      for (const number of numbers){
+            if(number < small){
+                  small = number;
+            }
+      }
+      return small;
+}
+const input = [34, 15, 88, 2, 55];
+const little = smallestNumber(input);
+console.log(little);
